72cd9472bc79c9be294a33e4c4d052802a996786
Daniel Ayekpa <ayekpad@gmail.com>
2019-03-12 17:47:24 +0000

    first commit

---
+++ android/app/src/main/java/com/con7ptoo/flutter_multi_textfield/MainActivity.java
@@ -0,0 +1,13 @@
+package com.con7ptoo.flutter_multi_textfield;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
